6448ef3fd317236eba5c3fe35ab9a639c73ba550
dinelka <newlakshan3@gmail.com>
2025-04-07 16:22:53 +0530

    test3

--- src/main/java/com/example/demo/config/ConfigOkHttp.java
+++ src/main/java/com/example/demo/config/ConfigOkHttp.java
@@ -6,10 +6,10 @@ import org.springframework.stereotype.Service;
 import java.io.IOException;
 @Service
 public class ConfigOkHttp {
-    private static final String URL = "https://www.weavy.com/docs/reference/api/users";
-
+    private static final String URL = "https://8015b5dbc0724d38882ac90397c27649.weavy.io/api/users";
+    private final String TOKEN = "wys_iIaS4EpdQ5tHqr3oi5wt0FI7ABqKR20IQASx";
+    
     private final OkHttpClient client = new OkHttpClient();
-
 
     public String createUser(String uid, String name) throws IOException {
         String json = """
@@ -18,18 +18,15 @@ public class ConfigOkHttp {
                   "name": "%s"
                 }
                 """.formatted(uid, name);
-
         RequestBody body = RequestBody.create(json, MediaType.get("application/json"));
-
         Request request = new Request.Builder()
                 .url(URL)
                 .post(body)
-
+                .addHeader("Authorization", "Bearer" + TOKEN)
+                .addHeader("Content-Type", "application/json")
                 .build();
-
-        try (Response response = client.newCall(request).execute()) {
-            return response.body().string();
-        }
+        Response response = client.newCall(request).execute();
+        return response.body().string();
     }
 
     public String listUsers() throws IOException {
@@ -37,10 +34,8 @@ public class ConfigOkHttp {
                 .url(URL)
                 .get()
                 .build();
-
-        try (Response response = client.newCall(request).execute()) {
-            return response.body().string();
-        }
+        Response response = client.newCall(request).execute();
+        return response.body().string();
     }
 
 
@@ -49,10 +44,8 @@ public class ConfigOkHttp {
                 .url(URL + "/" + id)
                 .get()
                 .build();
-
-        try (Response response = client.newCall(request).execute()) {
-            return response.body().string();
-        }
+        Response response = client.newCall(request).execute();
+        return response.body().string();
     }
 
     public String updateUser(String id, String name) throws IOException {
@@ -61,29 +54,24 @@ public class ConfigOkHttp {
                   "name": "%s"
                 }
                 """.formatted(name);
-
         RequestBody body = RequestBody.create(json, MediaType.get("application/json"));
-
         Request request = new Request.Builder()
                 .url(URL + "/" + id)
                 .patch(body)
-
                 .build();
-        try (Response response = client.newCall(request).execute()) {
-            return response.body().string();
-        }
+        Response response = client.newCall(request).execute();
+        return response.body().string();
     }
 
 
     public String deleteUser(String id) throws IOException {
         Request request = new Request.Builder()
                 .url(URL + "/" + id)
+                .addHeader("Authorization", "Bearer" + TOKEN)
                 .delete()
-
                 .build();
-        try (Response response = client.newCall(request).execute()) {
-            return response.body().string();
-        }
+        Response response = client.newCall(request).execute();
+        return response.body().string();
     }
 
 
